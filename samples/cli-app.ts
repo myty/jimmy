b8fadbabe0b21332c89f2091691e7906f74fdbd3
Michael Tyson <michaeltyson@outlook.com>
2022-01-26 19:58:30 -0500

    simplified cli example

--- samples/cli-app.ts
+++ samples/cli-app.ts
@@ -1,79 +1,43 @@
+// deno run --allow-net ./samples/cli-app.ts
+
 import { Request } from "../request.ts";
 import { Notification } from "../notification.ts";
 import { Mediator } from "../mediator.ts";
-import { parse } from "https://deno.land/std@0.61.0/flags/mod.ts";
-import {
-  format,
-  fromUnixTime,
-} from "https://deno.land/x/date_fns@v2.15.0/index.js";
-import AsciiTable from "https://deno.land/x/ascii_table/mod.ts";
 
-const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-
-interface ForecastResult {
-  city: string;
-  days: string[][];
+interface AdviceObject {
+  slip: {
+    id: number;
+    advice: string;
+  };
 }
 
-interface ForecastItem {
-  dt: string;
-  main: { temp: number };
-  weather: { description: string }[];
-}
+type Slip = AdviceObject["slip"];
 
-class ForecastRequest extends Request<Promise<ForecastResult>> {
-  constructor(public city: string) {
-    super();
-  }
-}
+class RandomAdviceRequest extends Request<Promise<Slip>> {}
 
-class ForecastDisplayNotification extends Notification {
-  constructor(public forecastResult: ForecastResult) {
+class AdviceReceivedNotification extends Notification {
+  constructor(public slip: Slip) {
     super();
   }
 }
 
 const mediator = new Mediator();
 
-mediator.handle(ForecastRequest, async (req) => {
-  const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${req.city}&units=metric&appid=${apiKey}`,
-  );
+mediator.handle(RandomAdviceRequest, async () => {
+  const res = await fetch(`https://api.adviceslip.com/advice`);
+  const data: AdviceObject = await res.json();
 
-  const data = await res.json();
-  const days = data.list.slice(0, 8).map((item: ForecastItem) => [
-    format(fromUnixTime(item.dt), "do LLL, k:mm", {}),
-    `${item.main.temp.toFixed(1)}C`,
-    item.weather[0].description,
-  ]);
-
-  return {
-    city: data.city.name,
-    days,
-  };
+  return data.slip;
 });
 
-mediator.handle(ForecastDisplayNotification, (notification) => {
-  const { forecastResult } = notification;
+mediator.handle(AdviceReceivedNotification, (notification) => {
+  const { advice } = notification.slip;
 
-  const table = AsciiTable.fromJSON({
-    title: `${forecastResult.city} Forecast`,
-    heading: ["Time", "Temp", "Weather"],
-    rows: forecastResult.days,
-  });
-
-  console.log(table.toString());
+  console.log(advice);
 
   return Promise.resolve();
 });
 
-const args = parse(Deno.args);
+const slip = await mediator.send(new RandomAdviceRequest());
 
-if (args.city === undefined) {
-  console.error("No city supplied");
-  Deno.exit();
-}
-
-const data = await mediator.send(new ForecastRequest(args.city));
-
-await mediator.publish(new ForecastDisplayNotification(data));
+await mediator.publish(new AdviceReceivedNotification(slip));
